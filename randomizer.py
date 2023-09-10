f60d077671aa570320701131a428bbde3036113f
Viktor Koryakov <129833186+ViktorKoryakov@users.noreply.github.com>
2023-09-10 17:19:38 +0300

    Added file of code
    Добавлен файл кода.

---
+++ randomizer.py
@@ -0,0 +1,36 @@
+# [ IMPORTING ] 
+import random
+
+# [ TITLE ]
+print("-"*30)
+print("Настройте рандомайзер под себя")
+print("-"*30)
+print()
+
+# [ DEFINE SETTINGS ]
+first_num = int(input("Введите первое число: "))
+second_num = int(input("Введите второе число: "))
+times = int(input("Введите кол-во чисел: "))
+if times > 1:
+    repeating = bool(input("Не повторять числа? (True - да, False - нет): "))
+print()
+
+# [ yet another TITLE ]
+print("-"*6)
+print("Ответ:")
+print("-"*6)
+print()
+
+# [ ANSWER ]
+if times > 1:
+    list = [11121] # DO NOT CHANGE
+    for i in range(times):
+        rand_num = random.randint(first_num, second_num)
+        if repeating == True:
+            while rand_num in list:
+                rand_num = random.randint(first_num, second_num)
+        list.append(rand_num)
+    print(f"Рандомные числа... {list}")
+else:
+    print(f"Рандомное число... {random.randint(first_num, second_num)}")
+    
