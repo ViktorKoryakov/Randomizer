01a001435bcb8bdf2782e5ec65cd9cab79fdcdc1
Viktor Koryakov <129833186+ViktorKoryakov@users.noreply.github.com>
2023-09-13 17:02:24 +0300

    Updated logic of the "repeating" variable. Minor code fixes.
    Обновлена (изменена) логика работы переменной "repeating". Небольшие исправления кода.

--- randomizer.py
+++ randomizer.py
@@ -2,9 +2,7 @@
 import random
 
 # [ TITLE ]
-print("-"*30)
-print("Настройте рандомайзер под себя")
-print("-"*30)
+print(f"{'-'*30} \nНастройте рандомайзер под себя \n{'-'*30}")
 print()
 
 # [ SETTINGS ]
@@ -12,25 +10,21 @@ first_num = int(input("Введите первое число: "))
 second_num = int(input("Введите второе число: "))
 times = int(input("Введите кол-во чисел: "))
 if times > 1:
-    repeating = bool(input("Не повторять числа? (True - да, False - нет): "))
+    repeating = bool(input("Повторять числа? (True - да, False - нет): "))
 print()
 
 # [ yet another TITLE ]
-print("-"*6)
-print("Ответ:")
-print("-"*6)
-print()
+print(f"{'-'*6} \nОтвет: \n{'-'*6}")
 
 # [ ANSWER ]
 if times > 1:
     list = [] # DO NOT CHANGE
     for i in range(times):
         rand_num = random.randint(first_num, second_num)
-        if repeating == True:
+        if repeating == (not False):
             while rand_num in list:
                 rand_num = random.randint(first_num, second_num)
         list.append(rand_num)
     print(f"Рандомные числа... {list}")
 else:
     print(f"Рандомное число... {random.randint(first_num, second_num)}")
-    
